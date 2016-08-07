f82ac8be8cfde1579f004df37cfe6be38501693a
dhrmnfce <dylanludwick@gmail.com>
2016-08-07 18:45:15 +0000

    Sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   resources :articles
+  resources :users
+  get 'signup' => 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
