b59fcc407da764dcae5cfb80db74fac691c2522d
dhrmnfce <dylanludwick@gmail.com>
2016-08-24 02:15:05 +0000

    Add admin to user with restricitons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
       end
       
       def require_same_user
-         if current_user != @article.user
+         if current_user != @article.user and !current_user.admin?
             flash[:danger] = "Don't do that"
             redirect_to root_path
          end
